6aaa49fbf82d6a9697bfe45030e9ca4528f356fb
Sudarshan Sunil Bankar <sudarshanbankar9@gmail.com>
2025-11-30 16:19:24 +0530

    Change MongoDB connection string to empty
    Updated MongoDB connection string to be empty.

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectToDB(){
-    mongoose.connect("mongodb+srv://bankarsudarshan6_db_user:LCSxSZGZXvXkE6DH@cluster0.ryidzl.mongodb.net/first").then(()=>{
+    mongoose.connect("").then(()=>{
         console.log("Connected to DB")
     }).catch((err)=>{
         console.log("Error connecting to DB",err)
